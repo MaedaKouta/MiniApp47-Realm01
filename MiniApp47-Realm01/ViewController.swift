c4b9c9678e8b684d57f162397389b396c1878aa9
Kota <maedakoutaa@gmail.com>
2022-04-24 14:00:02 +0900

    Initial Commit

---
+++ MiniApp47-Realm01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp47-Realm01
+//
+//  Created by 前田航汰 on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
